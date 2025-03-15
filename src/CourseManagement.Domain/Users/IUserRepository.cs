66a0907993413145c639e82cf4d20c6d43f4a0f9
Md. Ariful Islam <arifjmamun24@gmail.com>
2025-03-16 05:38:40 +0600

    update user repository interface

--- src/CourseManagement.Domain/Users/IUserRepository.cs
+++ src/CourseManagement.Domain/Users/IUserRepository.cs
@@ -1,7 +1,10 @@
+using CourseManagement.Domain.Users.ValueObjects;
+
 namespace CourseManagement.Domain.Users;
 
 public interface IUserRepository
 {
     Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<User?> GetByEmailAsync(Email email, CancellationToken cancellationToken = default);
     void Add(User user);
 }
