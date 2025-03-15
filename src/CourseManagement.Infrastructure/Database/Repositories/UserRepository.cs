8a854655b79ce022aa23223c71a3bcd311a0a64e
Md. Ariful Islam <arifjmamun24@gmail.com>
2025-03-16 05:48:46 +0600

    update user repository implementation

--- src/CourseManagement.Infrastructure/Database/Repositories/UserRepository.cs
+++ src/CourseManagement.Infrastructure/Database/Repositories/UserRepository.cs
@@ -1,7 +1,15 @@
 using CourseManagement.Domain.Users;
+using CourseManagement.Domain.Users.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseManagement.Infrastructure.Database.Repositories;
 
 internal sealed class UserRepository(ApplicationDbContext dbContext) : Repository<User>(dbContext), IUserRepository
 {
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    public Task<User?> GetByEmailAsync(Email email, CancellationToken cancellationToken = default)
+    {
+        return _dbContext.Users.FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
+    }
 }
