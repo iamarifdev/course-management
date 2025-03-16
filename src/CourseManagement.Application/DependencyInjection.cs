e6708d6dd939f418c5695bd649c3b6f62e5fa537
Md. Ariful Islam <arifjmamun24@gmail.com>
2025-03-16 13:14:34 +0600

    add validation pipeline behavior

--- src/CourseManagement.Application/DependencyInjection.cs
+++ src/CourseManagement.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using CourseManagement.Application.Base.Behaviors;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +11,8 @@ public static class DependencyInjection
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            
+            configuration.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
