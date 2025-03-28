33f73cd3c2f083a151dc2c45efd647e5cc35b064
Md. Ariful Islam <arifjmamun24@gmail.com>
2025-03-29 03:36:17 +0600

    add unit tests project

---
+++ test/CourseManagement.API.UnitTests/Users/UserData.cs
@@ -0,0 +1,21 @@
+namespace CourseManagement.API.UnitTests.Users;
+
+internal static class UserData
+{
+    internal static class Student
+    {
+        public const string Email = "student@test.com";
+        public const string Password = "12345";
+        public const string FirstName = "Test";
+        public const string LastName = "Student";
+    }
+
+    internal static class Staff
+    {
+        public const string Email = "stuff@test.com";
+        public const string Password = "12345";
+        public const string FirstName = "Test";
+        public const string LastName = "Staff";
+        public const string Department = "Test Department";
+    }
+}
